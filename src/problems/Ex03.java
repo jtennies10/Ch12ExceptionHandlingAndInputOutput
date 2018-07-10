6735d749aab42397ab35c3f4b908554c4416e8bc
Joshua Tennies <jdtennies10@gmail.com>
2018-07-10 17:06:57 -0400

    Solved Ex03

--- src/problems/Ex03.java
+++ src/problems/Ex03.java
@@ -1,8 +1,28 @@
 package problems;
 
-public class Ex03 {
-	private void psvm() {
-		// TODO Auto-generated method stub
+import java.util.Random;
+import java.util.Scanner;
 
+public class Ex03 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random rand = new Random();
+		
+		//create array and fill with random ints
+		int[] a = new int[100];
+		for(int i = 0; i < a.length; i++) {
+			a[i] = rand.nextInt();
+		}
+		
+		System.out.print("Enter index of array value to display: ");
+		
+		try {
+			//get user index choice
+			int index = sc.nextInt();
+			System.out.println(a[index]);
+		} catch(ArrayIndexOutOfBoundsException ae) {
+			//alert user the index is out of bounds
+			System.out.println("Index Out of Bounds");
+		}
 	}
 }
